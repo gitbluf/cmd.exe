7163b085b3bac35b7258b5d543d075978bb819d1
gitbluf <git+gh@gitbluf.xyz>
2026-05-08 22:36:15 +0200

    prune unused exports: SlotName, escapeShell, applyMode

--- src/commands/handlers/index.ts
+++ src/commands/handlers/index.ts
@@ -3,7 +3,7 @@
  */
 
 export { handleAsk } from "./ask";
-export { applyMode, handlePlan } from "./build";
+export { handlePlan } from "./build";
 export { handlePlanSave } from "./plan-save";
 export { handleRtk } from "./rtk";
 export { handleTodos } from "./todos";
